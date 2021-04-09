64af293af3b1a4019101f36ea1b425d3ce556694
Emile-yu <yjg9091@gmail.com>
2021-04-09 13:52:45 +0200

    Simplify the Tax Calculation

--- RetailManager/RetailWPFUserInterface/ViewModels/SalesViewModel.cs
+++ RetailManager/RetailWPFUserInterface/ViewModels/SalesViewModel.cs
@@ -135,10 +135,18 @@ namespace RetailWPFUserInterface.ViewModels
 
             decimal taxRate = _configHelper.GetTaxRate()/100;
 
-            foreach (var item in Cart)
-            {
-                taxAmount += item.Product.RetailPrice * item.QuantityInCart * taxRate;
-            }
+
+            taxAmount = Cart
+                .Where(x => x.Product.IsTaxable)
+                .Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate);
+            //foreach (var item in Cart)
+            //{
+            //    if (item.Product.IsTaxable)
+            //    {
+            //        taxAmount += item.Product.RetailPrice * item.QuantityInCart * taxRate;
+            //    }
+                
+            //}
 
             return taxAmount;
         }
